0b25944dcc4d7a56f11982457e99ace4ad463e93
Vasu Web Developer <kasipurivasu@gmail.com>
2022-03-09 17:18:55 +0530

    final

---
+++ project_1/app.ts
@@ -0,0 +1,15 @@
+const number1 = 5.9;
+const number2 = 9;
+
+
+function add(number1: number,number2: number){
+    return number1+number2;
+}
+
+var result = add(number1,number2);
+console.log(result);
+
+
+
+let sentence: string;
+sentence = "stringval";
