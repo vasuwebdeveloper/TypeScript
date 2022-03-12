07dea0f379ea426d98bec6529fb2ba3e15c95983
Vasu Web Developer <kasipurivasu@gmail.com>
2022-03-12 15:00:19 +0530

    Include and exclude files in the tsconfig.json file

--- project_1/watchtest.ts
+++ project_1/watchtest.ts
@@ -6,6 +6,17 @@ console.log("watch.ts");
  *  Working with multiple files watch mode ( tsc -- init(which creates the ts.config file to manage the watch mode for all the files))
  * Then click on tsc to compile the project folder
  *  For watch version of project folder (tsc --watch)
+ * 
+ * 
+ * 
+ * Exclude files from the automatic compilation
+ * Add exclude array in the tsconfig.json file and exclude the files
+ * 
+ * *.dev.ts (we can also add the extensions like this)
+ * 
+ * Add include array in the tsconfig.json file and include the files
+ * 
 */
+
 
 
